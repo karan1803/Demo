467da230402d1d189465fb9f0dd564a270f7cc8d
karan1803 <prabhak253@gmail.com>
2023-02-16 15:45:29 +0530

    table in right side done

--- src/app/table-view/table-view.component.ts
+++ src/app/table-view/table-view.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class TableViewComponent {
 
+  usersList = [
+    {
+      first: 'Karan',
+      last: 'Prabha',
+      handler: '@karan01'
+    },
+    {
+      first: 'Mani',
+      last: 'vasu',
+      handler: '@mani1'
+    },
+    {
+      first: 'Nathan',
+      last: 'Praga',
+      handler: '@praga031'
+    },
+    {
+      first: 'sam',
+      last: 'jerin',
+      handler: '@sam1'
+    }
+  ];
+
+  columnsList = [
+    {
+      header: "#",
+    }, 
+    {
+      header: "First",
+    }, 
+    {
+      header: "Last",
+    },
+    {
+      header: "Handle",
+    }
+  ];
+
 }
