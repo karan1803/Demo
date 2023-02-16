a3fb4a249c78a1a0c23c533d133a0834c95691a3
karan1803 <prabhak253@gmail.com>
2023-02-16 15:08:15 +0530

    component created for both

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './table-view/table-view.component';
+import { ChartViewComponent } from './chart-view/chart-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewComponent,
+    ChartViewComponent
   ],
   imports: [
     BrowserModule,
